c34b10c69f91c90b5830a92125c3baa07a3c8e23
Soundhar <laptopisasset@gmail.com>
2021-09-03 22:48:17 +0530

    Added Error Handler.

--- server/server.js
+++ server/server.js
@@ -10,4 +10,9 @@ app.use(express.json());
 
 routes(app);
 
+app.use((err, req, res, next) => {
+  res.send(err.status || 500).json({ errror: { message: err.message } });
+  next();
+});
+
 app.listen(PORT, () => console.log(`Listenting on port ${PORT}`));
