60242789047611759307014f33c4c837b906a15d
Soundhar <laptopisasset@gmail.com>
2021-09-03 22:02:24 +0530

    Basic server setup.

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Listenting on port ${PORT}`));
